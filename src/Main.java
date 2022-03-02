818ddf10ae6b160112cc49f839aa5b560378119d
Andrei Bumbaru <62432675+bumbaruandrei07@users.noreply.github.com>
2022-03-02 16:17:28 +0200

    init commit

--- src/Main.java
+++ src/Main.java
@@ -37,22 +37,76 @@ public class Main {
                     break;
 
                 case "add":
-                    System.out.println("Adauga o noua persoana (inscriere)");
+                    System.out.println("Adauga o noua persoana (inscriere): ");
 
                     newCommand();
                     break;
 
                 case "check":
-                    System.out.println("Verifica daca o persoana este inscrisa la eveniment");
+                    System.out.println("Verifica daca o persoana este inscrisa la eveniment: ");
+
+                    newCommand();
+                    break;
+
+                case "remove":
+                    System.out.println("Sterge o persoana existenta din lista: ");
+
+                    newCommand();
+                    break;
+
+                case "update":
+                    System.out.println("Actualizeaza detaliile unei persoane: ");
+
+                    newCommand();
+                    break;
+
+                case "guests":
+                    System.out.println("Lista de persoane care participa la eveniment: ");
+
+                    newCommand();
+                    break;
+
+                case "waitList":
+                    System.out.println("Persoanele din lista de asteptare: ");
+
+                    newCommand();
+                    break;
+
+                case "available":
+                    System.out.println("Numarul de locuri libere: ");
+
+                    newCommand();
+                    break;
+
+                case "guests_no":
+                    System.out.println("Numarul de persoane care participa la eveniment: ");
+
+                    newCommand();
+                    break;
+
+                case "waitlist_no":
+                    System.out.println("Numarul de persoane din lista de asteptare: ");
+
+                    newCommand();
+                    break;
+
+                case "subscribe_no":
+                    System.out.println("Numarul total de persoane inscrise: ");
+
+                    newCommand();
+                    break;
+
+                case "search":
+                    System.out.println("Cauta toti invitatii conform sirului de caractere introdus: ");
 
                     newCommand();
                     break;
             }
 
-
         }
 
-
+        sc.close();
     }
+
 
 }
